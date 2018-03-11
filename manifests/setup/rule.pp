b0b66001ff3d0f04eb60927805986cf8022b2ca1
Patrick Verzele <patrick@verzele.be>
2018-03-11 15:04:16 +0000

    Rules

--- manifests/setup/rule.pp
+++ manifests/setup/rule.pp
@@ -1,3 +1,33 @@
 define shorewall::setup::rule (
+  String $action,
+  String $source,
+  String $dest,
+  String $proto,
+  String $dport,
+  String $sport,
+  String $origdest,
+  String $rate,
+  String $user,
+  String $mark,
+  String $connlimit,
+  String $time
 ) {
+  if ! defined(Concat['/etc/shorewall/rules']) {
+    concat {'/etc/shorewall/rules':
+      ensure => present,
+      ensure_newline => true,
+      owner => root,
+      group => root,
+      mode => '0644',
+      path => '/etc/shorewall/rules',
+    }
+    concat::fragment {'/etc/shorewall/rule-header':
+      source => 'puppet:///modules/shorewall/rule-header'
+      target => '/etc/shorewall/rules',
+    }
+  }
+  concat::fragment {"rule-${title}"
+    content => template('shorewall/rule.erb'),
+    target => '/etc/shorewall/rules',
+  }
 }
