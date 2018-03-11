c01cf0471d0fec53be1ab15fd4409eb52ec36052
Patrick Verzele <patrick@verzele.be>
2018-03-11 15:04:01 +0000

    Zones

--- manifests/setup/zone.pp
+++ manifests/setup/zone.pp
@@ -1,3 +1,26 @@
 define shorewall::setup::zone (
+  String $zone,
+  Enum['ipv4', 'fw'] $type,
+  Array[String] $options,
+  Array[String] $in_options,
+  Array[String] $out_options
 ) {
+  if ! defined(Concat['/etc/shorewall/zones']) {
+    concat {'/etc/shorewall/zones':
+      ensure => present,
+      ensure_newline => true,
+      owner => root,
+      group => root,
+      mode => '0644',
+      path => '/etc/shorewall/zones',
+    }
+    concat::fragment {'/etc/shorewall/zone-header':
+      source => 'puppet:///modules/shorewall/zone-header'
+      target => '/etc/shorewall/zones',
+    }
+  }
+  concat::fragment {"zone-${title}"
+    content => template('shorewall/zone.erb'),
+    target => '/etc/shorewall/zones',
+  }
 }
