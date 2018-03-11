b80fb3a142d428707868643dddc61b2cd766f5e8
Patrick Verzele <patrick@verzele.be>
2018-03-11 15:04:35 +0000

    Policies

--- manifests/setup/policy.pp
+++ manifests/setup/policy.pp
@@ -1,3 +1,27 @@
 define shorewall::setup::policy (
+  String $source,
+  String $dest,
+  String $policy,
+  String $loglevel,
+  String $rate,
+  String $connlimit
 ) {
+  if ! defined(Concat['/etc/shorewall/policy']) {
+    concat {'/etc/shorewall/policy':
+      ensure => present,
+      ensure_newline => true,
+      owner => root,
+      group => root,
+      mode => '0644',
+      path => '/etc/shorewall/policy',
+    }
+    concat::fragment {'/etc/shorewall/policy-header':
+      source => 'puppet:///modules/shorewall/policy-header'
+      target => '/etc/shorewall/policy',
+    }
+  }
+  concat::fragment {"policy-${title}"
+    content => template('shorewall/policy.erb'),
+    target => '/etc/shorewall/policy',
+  }
 }
