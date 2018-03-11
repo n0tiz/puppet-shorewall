4640a7f3cbce0d6fd9b4b3bbb3292e5dd08762d8
Patrick Verzele <patrick@verzele.be>
2018-03-11 15:04:26 +0000

    Interfaces

--- manifests/setup/interface.pp
+++ manifests/setup/interface.pp
@@ -1,3 +1,24 @@
 define shorewall::setup::interface (
+  String $zone,
+  String $interface,
+  Array[String] $options
 ) {
+  if ! defined(Concat['/etc/shorewall/interfaces']) {
+    concat {'/etc/shorewall/interfaces':
+      ensure => present,
+      ensure_newline => true,
+      owner => root,
+      group => root,
+      mode => '0644',
+      path => '/etc/shorewall/interfaces',
+    }
+    concat::fragment {'/etc/shorewall/interfaces-header':
+      source => 'puppet:///modules/shorewall/interface-header'
+      target => '/etc/shorewall/interfaces',
+    }
+  }
+  concat::fragment {"interface-${title}"
+    content => template('shorewall/interface.erb'),
+    target => '/etc/shorewall/interfaces',
+  }
 }
