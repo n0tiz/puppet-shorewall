33ff0211aa6fba5c4331ca0a2f3a6120d4af34f4
Patrick Verzele <patrick@verzele.be>
2018-03-12 22:23:03 +0000

    Moving params to init

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,13 @@
-class shorewall inherits shorewall::params {
+class shorewall (
+  Boolean $package_manage,
+  Array[String] $package_name,
+  String $package_ensure,
+  Boolean $service_manage,
+  String $service_name,
+  Optional[String] $service_provider,
+  Boolean $service_enable,
+  Enum['running', 'stopped'] $service_ensure
+) {
   contain shorewall::install
   contain shorewall::config
   contain shorewall::service
