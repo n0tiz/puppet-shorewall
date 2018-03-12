64b2ec2dd056fb74cfd5a0a259e58f89e46e3348
Patrick Verzele <patrick@verzele.be>
2018-03-12 22:26:34 +0000

    Fixing config

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,20 +1,20 @@
 class shorewall::config {
   file {'/etc':
-    path => '/etc/shorewall/shorewall.conf'
+    path => '/etc',
     ensure => directory,
     group => root,
     mode => '0755',
     owner => root,
   }
   file {'/etc/shorewall':
-    path => '/etc/shorewall/shorewall.conf'
+    path => '/etc/shorewall',
     ensure => directory,
     group => root,
     mode => '0755',
     owner => root,
   }
   file {'/etc/shorewall/shorewall.conf':
-    path => '/etc/shorewall/shorewall.conf'
+    path => '/etc/shorewall/shorewall.conf',
     ensure => file,
     source => 'puppet:///modules/shorewall/shorewall-config',
     group => root,
