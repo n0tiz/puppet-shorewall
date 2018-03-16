27f2f0a11f3c6bba872392eaf3d3e04aa1aa06c0
Patrick Verzele <patrick@verzele.be>
2018-03-16 21:58:25 +0000

    /etc and /etc/shorewall should be created by installing the package

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,18 +1,4 @@
 class shorewall::config {
-  file {'/etc':
-    path => '/etc',
-    ensure => directory,
-    group => root,
-    mode => '0755',
-    owner => root,
-  }
-  file {'/etc/shorewall':
-    path => '/etc/shorewall',
-    ensure => directory,
-    group => root,
-    mode => '0755',
-    owner => root,
-  }
   file {'/etc/shorewall/shorewall.conf':
     path => '/etc/shorewall/shorewall.conf',
     ensure => file,
