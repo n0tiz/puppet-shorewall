edff014d28643762685b1212449c42ade9638e6e
Patrick Verzele <patrick@verzele.be>
2018-04-08 22:02:28 +0100

    Adding tunnel functionality

---
+++ manifests/config/tunnel.pp
@@ -0,0 +1,35 @@
+define shorewall::config::tunnel (
+  String $type,
+  String $zone,
+  String $gateway,
+  String $gateway_zone
+) {
+  if $shorewall::service_manage == true {
+    $notify = Service['shorewall']
+  } else {
+    $notify = undef
+  }
+  if ! defined(Concat['/etc/shorewall/tunnels']) {
+    concat {'/etc/shorewall/tunnels':
+      ensure => present,
+      ensure_newline => true,
+      owner => root,
+      group => root,
+      mode => '0644',
+      path => '/etc/shorewall/tunnels',
+      notify => $notify,
+    }
+    concat::fragment {'tunnels-header':
+      source => 'puppet:///modules/shorewall/tunnel-header',
+      target => '/etc/shorewall/tunnels',
+      order => 1,
+      notify => $notify,
+    }
+  }
+  concat::fragment {"tunnel-${title}":
+    content => template('shorewall/tunnel.erb'),
+    target => '/etc/shorewall/tunnels',
+    order => 2,
+    notify => $notify,
+  }
+}
